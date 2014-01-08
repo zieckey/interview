e39b69887adddabec243b0860acc2b359958cb01
zieckey wei 360desktop <zieckey@gmail.com>
2014-01-08 15:27:01 +0800

    add vector

---
+++ vector/qh_vector.h
@@ -0,0 +1,35 @@
+#ifndef QIHOO_STRING_H_
+#define QIHOO_STRING_H_
+
+namespace qh
+{
+    class string {
+    public:
+        //ctor
+        string();
+        string(const char* s);
+        string(const char* s, size_t len);
+        string(const string& rhs);
+
+        string& operator=(const string& rhs);
+
+        //dtor
+        ~string();
+
+        //get
+        size_t size() const;
+        const char* data() const;
+        const char* c_str() const;
+
+        // set & get
+        char* operator[](size_t index);
+
+    private:
+        char*  data_;
+        size_t len_;
+    };
+}
+
+#endif
+
+
