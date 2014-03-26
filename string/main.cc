8a1d615071eb74ca24819bc487ccd07881959bdb
weizili.build9 <zieckey@gmail.com>
2014-03-26 16:51:43 +0800

    Add more comments

--- string/main.cc
+++ string/main.cc
@@ -6,6 +6,7 @@
 int main(int argc, char* argv[])
 {
     //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
+    //TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
 
 #ifdef WIN32
     system("pause");
--- vector/main.cc
+++ vector/main.cc
@@ -5,6 +5,7 @@
 int main(int argc, char* argv[])
 {
     //TODO 在这里添加单元测试，越多越好，代码路径覆盖率越全越好
+    //TODO 单元测试写法请参考INIParser那个项目，不要写一堆printf，要用assert进行断言判断。
 
     qh::vector<int> num_vect;
     num_vect.push_back(1);
