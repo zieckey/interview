d4b0490e1042daac90fd73d53e7df2d5254de1cf
weizili.laptop <zieckey@gmail.com>
2015-01-28 10:34:50 +0800

    add test data

--- climber_problem/main.cc
+++ climber_problem/main.cc
@@ -1,7 +1,35 @@
 #include <stdio.h>
+#include <assert.h>
+
+#define H_ARRAYSIZE(a) \
+    ((sizeof(a) / sizeof(*(a))) / \
+    static_cast<size_t>(!(sizeof(a) % sizeof(*(a)))))
+
+int resolve(const char* input)
+{
+    return 0;
+}
 
 int main(int argc, char* argv[]) 
 {
-    // Enter your code here. Read input from STDIN. print output to STDOUT
+    const char* input[] = {
+        "3\n1,3,2\n2,4,4\n6,7,5\n", //The giving example
+        "1\n1,2,1\n",
+        "2\n1,2,1\n2,3,2",
+        "3\n1,2,1\n2,3,2\n3,6,1",
+        "4\n1,2,1\n2,3,2\n3,6,1\n5,8,2",
+        "5\n1,2,1\n2,3,2\n3,6,1\n5,8,2\n7,9,1",
+        "1\n0,1,1",
+        "2\n0,1,1\n2,4,3",
+        "3\n0,1,1\n2,4,3\n3,5,1",
+        "4\n0,1,1\n2,4,3\n3,5,1\n5,6,1",
+        "5\n0,1,1\n2,4,3\n3,5,1\n5,6,1\n6,8,3",
+        //TODO please add more test case here
+        };
+    int expectedSteps[] = {25, 4, 7, 10, 14, 15, 3, 12, 13, 14, 20};
+    for (size_t i = 0; i < H_ARRAYSIZE(input); ++i)
+    {
+        assert(resolve(input[i]) == expectedSteps[i]);
+    }
     return 0;
 }
